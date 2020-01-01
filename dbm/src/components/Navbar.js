2e056d2ebf07b77a0881b82d49314a382c31247e
Arpan7310 <arpanchrome@gmail.com>
2020-01-01 18:22:50 +0530

    navbar

--- dbm/src/components/Navbar.js
+++ dbm/src/components/Navbar.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React ,{Component} from 'react'
+import M from 'materialize-css'
+class Navbar extends Component {
 
-function Navbar() {
+componentDidMount(){
+  M.AutoInit();
+}
+
+
+
+  render(){
     return(
+      <div>
          <nav>
            <div class="navbar-fixed">
     <div class="nav-wrapper   purple lighten-1 ">
-      <a href="#" class="brand-logo">Database</a>
+      <a href="#!" class="brand-logo">Database</a>
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a href="/">Home</a></li>
         <li><a href="/Addnew">Add Product</a></li>
@@ -16,7 +26,19 @@ function Navbar() {
     </div>
     </div>
   </nav>
+  <ul class="sidenav" id="mobile-demo">
+    <li><a href="/">Home</a></li>
+    <li><a href="/Addnew">Add Product</a></li>
+    <li><a href="/Card2">View Product</a></li>
+    <li><a href="/Card3">Search Records</a></li>
+  </ul>
+
+
+
+
+  </div>
     )
+}
 }
 
 export default Navbar;
